0d1eb4eadf68dc21f3d3f6da38cb7d1584ee8b9f
tuh8888 <harrison.pielke-lombardo@cuanschutz.edu>
2020-06-24 06:47:48 -0600

    Allow setting arbitrary fields in colors

--- src/oz_vega_utils/core.clj
+++ src/oz_vega_utils/core.clj
@@ -64,13 +64,12 @@
   "Add colors to the specified mark.
 
   Provides: sym"
-  [vega mark {:keys [data field type scheme stroke fill strokeWidth] :or {scheme "category20c"}}]
+  [vega mark {:keys [params data field type  scheme stroke] :or {scheme "category20c"}}]
   (let [sym (prop-sym vega :colors mark)]
     (if (= :static type)
-      (-> vega
-        (util/assoc-in-with-kv-index [:marks [:name mark] :encode :update :fill :value] fill)
-        (util/assoc-in-with-kv-index [:marks [:name mark] :encode :update :stroke :value] stroke)
-        (util/assoc-in-with-kv-index [:marks [:name mark] :encode :update :strokeWidth :value] strokeWidth))
+      (reduce (fn [vega [k v]]
+                (util/assoc-in-with-kv-index vega [:marks [:name mark] :encode :update k :value] v))
+        vega params)
       (-> vega
         (update :scales conj {:name   sym
                               :type   type
